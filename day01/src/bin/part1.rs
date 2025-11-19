582689cb54a1d2a4f4d6c18bd87dbabc55bdfb2a
matason <chrispmaiden@gmail.com>
2025-11-19 11:29:41 +0000

    Move source files into bin directory

--- day01/src/bin/part1.rs
+++ day01/src/bin/part1.rs
@@ -13,14 +13,14 @@ impl Me {
                 Direction::East => Direction::North,
                 Direction::South => Direction::East,
                 Direction::West => Direction::South,
-            }
+            },
             "R" => match self.direction {
                 Direction::North => Direction::East,
                 Direction::East => Direction::South,
                 Direction::South => Direction::West,
                 Direction::West => Direction::North,
-            }
-            _ => panic!()
+            },
+            _ => panic!(),
         };
 
         let distance = instruction.1.parse::<i32>().unwrap();
@@ -42,20 +42,23 @@ enum Direction {
 }
 
 fn main() {
-    let input = include_str!("../input.txt");
-    println!("{}", part1(input.trim()))
+    let input = include_str!("../../input.txt");
+    println!("{}", run(input.trim()))
 }
 
-fn part1(input: &str) -> i32 {
+fn run(input: &str) -> i32 {
     let mut me = Me {
         x: 0,
         y: 0,
         direction: Direction::North,
     };
 
-    let _ = input.split(", ").map(|instruction| {
-        me.r#move(instruction.split_at(1));
-    }).collect::<Vec<_>>();
+    let _ = input
+        .split(", ")
+        .map(|instruction| {
+            me.r#move(instruction.split_at(1));
+        })
+        .collect::<Vec<_>>();
 
     ((0 - me.x) + (0 - me.y)).abs()
 }
@@ -66,16 +69,16 @@ mod tests {
 
     #[test]
     fn five() {
-        assert_eq!(5, part1("R2, L3"));
+        assert_eq!(5, run("R2, L3"));
     }
 
     #[test]
     fn two() {
-        assert_eq!(2, part1("R2, R2, R2"));
+        assert_eq!(2, run("R2, R2, R2"));
     }
 
     #[test]
     fn twelve() {
-        assert_eq!(12, part1("R5, L5, R5, R3"));
+        assert_eq!(12, run("R5, L5, R5, R3"));
     }
 }
